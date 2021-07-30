56a0b34b9e5ed69467830c6e29b6ea637cf3c1aa
DavidMBruening <76656332+DavidMBruening@users.noreply.github.com>
2021-07-30 18:22:52 -0400

    Add files via upload

---
+++ Models/contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace ASPNetFrmwrkRapid.Models {
+    public class contact {
+        public string companyName { get; set; }
+        public int yearsInBusiness { get; set; }
+        public string contactName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
